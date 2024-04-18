e547be8b07c85526b41c7ab44ad8d1a16e863404
Mateus Gotardi <98918812+mateus-gotardi@users.noreply.github.com>
2024-04-18 15:54:55 -0300

    change max w

--- src/components/popular.tsx
+++ src/components/popular.tsx
@@ -8,10 +8,10 @@ export default function Popular() {
     const products = allProducts.slice(-3);
     return (
         <div className={cn(flexRow("c", "c"), "w-screen max-w-[1920px] lg:mb-[-6rem] lg:mt-0 mt-12 mb-12")}>
-            <div className={cn("")}>
+            <div className={cn("max-w-[1920px]")}>
                 <h2 className={cn("lg:relative lg:top-[-18rem] lg:mb-5 lg:ml-16 lg:pl-2 w-full text-center lg:text-left", subtitle)}>Popular <span className={cn(underlined)}>Now</span></h2>
-                <div className={cn("bg-accents h-28 w-[95vw] rounded-t-[3rem] relative top-[-6rem] left-[4px] hidden lg:block")}></div>
-                <div className={cn("bg-accents h-32 w-[94vw] rounded-t-[4rem] rotate-[4deg] origin-bottom-right relative top-[-11rem] hidden lg:block")}>
+                <div className={cn("bg-accents h-28 w-[95vw] max-w-[1920px] rounded-t-[3rem] relative top-[-6rem] left-[4px] hidden lg:block")}></div>
+                <div className={cn("bg-accents h-32 w-[94vw] max-w-[1920px] rounded-tr-[4rem] rotate-[4deg] origin-bottom-right relative top-[-11rem] hidden lg:block")}>
                 </div>
                 <div className={cn("lg:bg-accents h-fit lg:h-52 w-[95vw] rounded-b-[4rem] lg:relative top-[-10rem] lg:top-[-14rem] left-[4px] p-10 flex-wrap lg:flex-nowrap gap-7", flexRow("s", "a"))}>
                     {products.map((product, index) => {
